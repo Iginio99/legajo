0a16a363ba2d5f1f632a823d280ebb8a0a6d3102
unknown <iginio99gamboa@gmail.com>
2023-03-03 15:52:11 -0500

    Inicio del sitio web

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+                    
                     @role('admin')
                         <x-nav-link :href="url('add-docente')" :active="request()->routeIs('add-docente')">
                             {{ __('Agregar Docente') }}
